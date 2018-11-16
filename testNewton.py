595dccea2a378c5e4d027743f9c53717ead9be98
Cristian Lacey <clacey@princeton.edu>
2018-11-16 16:11:56 -0500

    Added testQuadratic() method to testNewton. Since the "x + h" error was already caught, this test automatically passes.

--- testNewton.py
+++ testNewton.py
@@ -28,6 +28,31 @@ class TestNewton(unittest.TestCase):
         # assertEqual rather than assertAlmostEqual
         self.assertEqual(x, -2.0)
 
+    def testQuadratic(self):
+        # Testing parabola of the form y = x^2 -1, where we'd
+        # expect roots at x = +-1. Tests starting points
+        # on either side of each of the two roots to ensure
+        # the closest root is found.
+
+        f = lambda x : x**2 - 1
+        solver = newton.Newton(f, tol=1.e-8, maxiter=100)
+
+        # Taking root1 to be x = -1, iterate over several
+        # initial conditions to the left of zero, where
+        # the closest root is x = -1.
+        root1_init_cond = np.linspace(-1000.0,-0.1,100)
+        for init in root1_init_cond:
+            x1 = solver.solve(init)
+            self.assertAlmostEqual(x1, -1.0)
+
+        # Taking root2 to be x = 1, iterate over several
+        # initial conditions to the right of zero, where
+        # the closest root is x = 1.
+        root2_init_cond = np.linspace(0.1,1000.0,100)
+        for init in root2_init_cond:
+            x2 = solver.solve(init)
+            self.assertAlmostEqual(x2, 1.0)
+        
 if __name__ == "__main__":
     unittest.main()
 
